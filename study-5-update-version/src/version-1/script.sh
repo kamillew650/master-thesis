d006a7e7c652b063c49f07cf51d2caf15320799d
= <=>
2022-07-22 00:05:17 +0200

    feat: create new stady docker images

---
+++ study-5-update-version/src/version-1/script.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+echo v1-$(date +"%T.%6N");
+
+ while true; do
+          sleep 5;
+          done
---
+++ study-5-update-version/src/version-2/script.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+echo v2-$(date +"%T.%6N");
+
+ while true; do
+          sleep 5;
+          done
