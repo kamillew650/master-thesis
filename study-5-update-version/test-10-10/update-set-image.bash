6c0cb57307beeab5b0edb8f3ffe633db26607b87
= <=>
2022-08-18 17:35:42 +0200

    fix update test

--- study-5-update-version/test-10-10/update-set-image.bash
+++ study-5-update-version/test-10-10/update-set-image.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 echo $(date +"%S,%6N");
-kubectl set image deployment/deployment-update container=deployment:2
+kubectl set image deployment/deployment-update container-1=deployment:2 container-2=deployment:2 container-3=deployment:2 container-4=deployment:2 container-5=deployment:2 container-6=deployment:2 container-7=deployment:2 container-8=deployment:2 container-9=deployment:2 container-10=deployment:2;
 
 
--- study-5-update-version/test-5-1/update-set-image.bash
+++ study-5-update-version/test-5-1/update-set-image.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 echo $(date +"%S,%6N");
-kubectl set image deployment/test-deployment-update container=deployment:2
+kubectl set image deployment/test-deployment-update container-1=deployment:2;
 
 
