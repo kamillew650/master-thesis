f6b1ae6011ccb038b2a31325f46dabcb21b56081
kamil <lewandowski.k1324@gmail.com>
2022-08-18 19:53:27 +0200

    minikube  update study results

--- study-5-update-version/run-study-kind.bash
+++ study-5-update-version/run-study-kind.bash
@@ -1,33 +1,51 @@
 #!/bin/bash
 
 variants1=(10);
-variants2=(10 20);
+variants2=(5 10);
 
-variants=(1 5 10 20);
+variants=(1 5 10);
 series=(1 2 3);
 
-for v1 in ${variants[@]}; do
-  for v2 in ${variants[@]}; do
+outputFile="result-kind.txt";
+
+for v1 in ${variants1[@]}; do
+  for v2 in ${variants2[@]}; do
+    cd ./"test-${v1}-${v2}";
+
     for s in ${series[@]}; do
-    
       echo $v1;
-      echo $v2;
-      cd ./"test-${v1}-${v2}";
+      echo $v2;   
 
-      echo "start run ${s}" >> ./result-kind.txt;
+      echo "start apply run ${s}" >> ./"${outputFile}";
 
-      startTime=$(./run-test.bash | head -n 1);
+      ./start-test.bash;
 
-      # ./run-test.bash >> ./result.txt;
+      sleep 5;
 
       resultLines=`../get-all-logs.bash | wc -l`;
       requiredAmountOfLines=$(($v1 * $v2));
 
+      echo $resultLines;
+      echo $requiredAmountOfLines;
+      while ((resultLines != requiredAmountOfLines)); do
+        echo "waiting for start-test to finish";
+        sleep 5s;
+        resultLines=`../get-all-logs.bash | wc -l`;
+      done;
+
+      startTime=$(./update-apply.bash | head -n 1);
+
+      # ./run-test.bash >> ./result.txt;
+
+
       sleep 5;
+
+      resultLines=`../get-all-logs.bash | wc -l`;
+      requiredAmountOfLines=$(($v1 * $v2));
 
       echo $resultLines;
       echo $requiredAmountOfLines;
-      while ((resultLines < requiredAmountOfLines)); do
+      while ((resultLines != requiredAmountOfLines)); do
         echo "waiting for get-logs to finish";
         sleep 5s;
         resultLines=`../get-all-logs.bash | wc -l`;
@@ -35,23 +53,141 @@ for v1 in ${variants[@]}; do
 
       endTime=$(../get-logs.bash);
 
-      echo "= ${endTime} - ${startTime}" >> ./result-kind.txt;
-      echo "------------" >> ./result-kind.txt;
-      ../get-all-logs.bash >> ./result-kind.txt;
+      echo "= ${endTime} - ${startTime}" >> ./"${outputFile}";
+      echo "------------" >> ./"${outputFile}";
+      ../get-all-logs.bash >> ./"${outputFile}";
 
       ../clean-up.bash;
 
       sleep 3;
 
-      cleanUpResult=`../get-logs.bash | grep "not found"`;
+      cleanUpResult=`../get-logs.bash | grep "No resources found in default namespace."`;
 
       while ! $cleanUpResult; do
         echo "waiting for clean-up to finish";
         sleep 1;
-        cleanUpResult=`../get-logs.bash | grep "not found"`;
+        cleanUpResult=`../get-logs.bash | grep "No resources found in default namespace."`;
+      done;
+    done
+
+    echo "---------------------------------------------------" >> ./"${outputFile}";
+
+    for s in ${series[@]}; do
+      echo $v1;
+      echo $v2;   
+
+      echo "start script run ${s}" >> ./"${outputFile}";
+
+      ./start-test.bash;
+
+      sleep $v2;
+
+      resultLines=`../get-all-logs.bash | wc -l`;
+      requiredAmountOfLines=$(($v1 * $v2));
+
+      echo $resultLines;
+      echo $requiredAmountOfLines;
+      while ((resultLines != requiredAmountOfLines)); do
+        echo "waiting for start-test to finish";
+        sleep 5s;
+        resultLines=`../get-all-logs.bash | wc -l`;
       done;
 
-      cd ..;
+      startTime=$(./update-script.bash | head -n 1);
+
+      sleep 10;
+
+      resultLines=`../get-all-logs.bash | wc -l`;
+      requiredAmountOfLines=$(($v1 * $v2));
+
+      echo $resultLines;
+      echo $requiredAmountOfLines;
+      while ((resultLines != requiredAmountOfLines)); do
+        echo "waiting for get-logs to finish";
+        echo "${resultLines}, ${requiredAmountOfLines}"
+        sleep 5s;
+        resultLines=`../get-all-logs.bash | wc -l`;
+      done;
+
+      endTime=$(../get-logs.bash);
+
+      echo "= ${endTime} - ${startTime}" >> ./"${outputFile}";
+      echo "------------" >> ./"${outputFile}";
+      ../get-all-logs.bash >> ./"${outputFile}";
+
+      ../clean-up.bash;
+
+      sleep 3;
+
+      cleanUpResult=`../get-logs.bash | grep "No resources found in default namespace."`;
+
+      while ! $cleanUpResult; do
+        echo "waiting for clean-up to finish";
+        sleep 1;
+        cleanUpResult=`../get-logs.bash | grep "No resources found in default namespace."`;
+      done;
     done
+
+    echo "---------------------------------------------------" >> ./"${outputFile}";
+
+    for s in ${series[@]}; do
+      echo $v1;
+      echo $v2;   
+
+      echo "start set-image run ${s}" >> ./"${outputFile}";
+
+      ./start-test.bash;
+
+      sleep $v2;
+
+      resultLines=`../get-all-logs.bash | wc -l`;
+      requiredAmountOfLines=$(($v1 * $v2));
+
+      echo $resultLines;
+      echo $requiredAmountOfLines;
+      while ((resultLines != requiredAmountOfLines)); do
+        echo "waiting for start-test to finish";
+        sleep 5s;
+        resultLines=`../get-all-logs.bash | wc -l`;
+      done;
+
+      startTime=$(./update-set-image.bash | head -n 1);
+
+      # ./run-test.bash >> ./result.txt;
+
+
+      sleep 10;
+
+      resultLines=`../get-all-logs.bash | wc -l`;
+      requiredAmountOfLines=$(($v1 * $v2));
+
+      echo $resultLines;
+      echo $requiredAmountOfLines;
+      while ((resultLines != requiredAmountOfLines)); do
+        echo "values ${resultLines} ${requiredAmountOfLines}";
+        echo "waiting for get-logs to finish";
+        sleep 5s;
+        resultLines=`../get-all-logs.bash | wc -l`;
+      done;
+
+      endTime=$(../get-logs.bash);
+
+      echo "= ${endTime} - ${startTime}" >> ./"${outputFile}";
+      echo "------------" >> ./"${outputFile}";
+      ../get-all-logs.bash >> ./"${outputFile}";
+
+      ../clean-up.bash;
+
+      sleep 3;
+
+      cleanUpResult=`../get-logs.bash | grep "No resources found in default namespace."`;
+
+      while ! $cleanUpResult; do
+        echo "waiting for clean-up to finish";
+        sleep 1;
+        cleanUpResult=`../get-logs.bash | grep "No resources found in default namespace."`;
+      done;
+    done
+    cd ..;
   done
 done
